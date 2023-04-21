abd83ed0a072c1acb927dd1d59e5188e6420f867
Ayşe Şimal Menekşe <94192946+simalmenekse@users.noreply.github.com>
2023-04-21 17:29:48 +0300

    Add files via upload

---
+++ Queues/Double Ended Queue/Queue using Two Stacks.cpp
@@ -0,0 +1,140 @@
+#include <iostream>
+
+using namespace std;
+class DEQueue{
+    private:
+        int front;
+        int back;
+        int size;
+        int *Q;
+    public:
+        DEQueue(int size);
+        ~DEQueue();
+        void display();
+        void enqueueFront(int x);
+        void enqueueBack(int x);
+        int dequeueFront();
+        int dequeueBack();
+        bool isEmpty();
+        bool isFull();
+};
+
+DEQueue::DEQueue(int size){
+    this->size = size;
+    front = -1;
+    back = -1;
+    Q = new int[size];
+}
+
+DEQueue::~DEQueue(){
+    delete []Q;
+}
+
+bool DEQueue::isEmpty(){
+    if(front==back)
+        return true;
+    return false;
+}
+
+bool DEQueue::isFull(){
+    if(back = size-1)
+        return true;
+    return false;
+
+}
+
+void DEQueue::enqueueFront(int x){
+    if(front== -1)
+        cout << "DEQueue Overflow!" << endl;
+    else{
+        Q[front] = x;
+        front--;
+    }
+}
+
+void DEQueue::enqueueBack(int x){
+    if(isFull())
+        cout << "DEQueue Overflow!" << endl;
+    else{
+        back++;
+        Q[back] = x;
+    }
+}
+
+int DEQueue::dequeueFront(){
+    int x=-1;
+    if(isEmpty())
+        cout << "DEQueue Underflow!" << endl;
+    else{
+        x = Q[front];
+        front++;
+    }
+
+    return x;
+}
+
+int DEQueue::dequeueBack(){
+    int x=-1;
+    if(back == -1)
+        cout << "DEQueue Underflow!" << endl;
+    else{
+        x = Q[back];
+        back--;
+    }
+
+    return x;
+}
+
+void DEQueue::display(){
+    for(int i=front+1; i<=back; i++){
+        cout << Q[i] << flush;
+        if(i < back)
+            cout << "<-" << flush;
+    }
+
+    cout << endl;
+}
+
+
+
+
+
+int main() {
+
+    int A[]= {1,3,5,7,9};
+    int B[] = {2,4,6,8};
+
+    DEQueue deq(sizeof(A)/sizeof(A[0]));
+
+    for(int i=0; i< sizeof(A)/sizeof(A[0]); i++)
+        deq.enqueueBack(A[i]);
+
+    deq.display();
+    deq.enqueueBack(11);
+
+    for(int i=0; i< sizeof(A)/sizeof(A[0]); i++)
+        deq.dequeueFront();
+    
+
+    deq.dequeueFront();
+
+    cout << endl;
+
+    for(int i=0; i< sizeof(B)/sizeof(B[0]); i++)
+        deq.enqueueFront(B[i]);
+    
+    deq.display();
+    deq.enqueueFront(10);
+    deq.enqueueFront(12);
+
+    for(int i=0; i< sizeof(B)/sizeof(B[0]);i++)
+        deq.dequeueBack();
+    
+    deq.display();
+    deq.dequeueBack();
+    deq.dequeueBack();
+
+
+ 
+    return 0;
+}
